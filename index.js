db7bf33166226e1360514ea8f2e838d716855c4a
Roberto Monplaisir <castedcrown84@gmail.com>
2021-11-24 09:33:48 -0500

    added inquirer promt

--- index.js
+++ index.js
@@ -25,3 +25,41 @@ const generateHTML = (answers) =>
 </div>
 </body>
 </html>`;
+
+
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
+    },
+    {
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ])
+
+
